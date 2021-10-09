92c1fe58b0f51b808c739e80d3eeb7e8253a52d3
mohammadn0man <mohammad.noman@live.in>
2021-10-09 14:06:26 +0530

    minimum backend requirement meet

--- src/main/java/com/social/Social/service/FollowRecordService.java
+++ src/main/java/com/social/Social/service/FollowRecordService.java
@@ -1,10 +1,13 @@
 package com.social.Social.service;
 
+import com.social.Social.dto.FollowRecordDto;
 import com.social.Social.model.FollowRecord;
 import com.social.Social.repository.FollowRecordRepository;
+import com.social.Social.util.MapperUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,4 +28,36 @@ public class FollowRecordService {
         }
         return result;
     }
+
+    public boolean follow(FollowRecordDto followRecordDto) {
+        if (followRecordRepository.existsByFollowedByUserIdAndFollowedToUserId(followRecordDto.getFollowedByUserId(), followRecordDto.getFollowedToUserId())) {
+            return true;
+        }
+        var followModel = MapperUtil
+                .getModelMapper()
+                .map(followRecordDto, FollowRecord.class);
+        try {
+            followRecordRepository.save(followModel);
+        } catch (Exception e) {
+            log.error("follower not saved");
+            return false;
+        }
+        return true;
+    }
+
+//    public boolean unfollow(FollowRecordDto followRecordDto) {
+//        if (!followRecordRepository.existsByFollowedByUserIdAndFollowedToUserId(followRecordDto.getFollowedByUserId(), followRecordDto.getFollowedToUserId())) {
+//            return true;
+//        }
+//        var followModel = MapperUtil
+//                .getModelMapper()
+//                .map(followRecordDto, FollowRecord.class);
+//        try {
+//            followRecordRepository.delete(followModel);
+//        } catch (Exception e) {
+//            log.error("changes to unfollow not saved");
+//            return false;
+//        }
+//        return true;
+//    }
 }
